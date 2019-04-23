fc147b07a1b2df37dab6bb1dd192e0bef0a93525
Sergii Repin <repin.sergey@gmail.com>
2019-04-23 07:54:22 +0300

    add router

---
+++ Controller/Account/Status.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Repin\CustomerStatus\Controller\Account;
+
+class Status extends \Magento\Customer\Controller\AbstractAccount
+{
+    public function execute()
+    {
+        die('status page');
+    }
+}
